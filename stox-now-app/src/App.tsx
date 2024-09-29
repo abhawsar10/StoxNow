fa1ad49a9b4b1bacdacfc1228c4e2c9d1063107c
Ankit Bhawsar <bhawsarankit10@gmail.com>
2024-09-28 18:40:32 -0700

    Add navigation links in header

--- stox-now-app/src/App.tsx
+++ stox-now-app/src/App.tsx
@@ -2,18 +2,32 @@ import React from 'react';
 import './App.css';
 import StockSearch from './components/StockSearch/StockSearch';
 import Portfolio from './components/Portfolio/Portfolio';
+import { Route,  Routes, Link, useLocation } from 'react-router-dom';
+import { FaSearch, FaChartLine } from 'react-icons/fa';
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>StoxNow</h1>
-      </header>
-      <main>
-        <StockSearch />
-        <Portfolio />
-      </main>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <a href='/'><h1>StoxNow</h1></a>
+          <nav>
+            <Link to="/search" title="Search Stock" className={location.pathname === "/search" || location.pathname === "/"? "active" : ""}>
+              <FaSearch size={24} />
+            </Link>
+            <Link to="/portfolio" title="View Portfolio" className={location.pathname === "/portfolio" ? "active" : ""}>
+              <FaChartLine size={24} />
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/search" element={<StockSearch />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/" element={<StockSearch />} />
+          </Routes>
+        </main>
+      </div>
   );
 }
 
